d1b9ab7d564653f54accb1984215872dacbe8268
Simone Fumagalli <simone.fumagalli@musement.com>
2019-06-07 14:20:46 +0200

    Added TypeString

---
+++ src/Types/TypeString.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Musement\JsonSchema\Types;
+
+class TypeString extends JsonSchemaType
+{
+
+	private $minLength;
+	private $maxLength;
+	private $patter;
+
+	// Taken from https://github.com/opis/json-schema/blob/19868514d5e4c27553b21c7f0cf6388734c67d18/src/Validator.php
+	const BELL = "\x07";
+
+	public function __construct(string $description)
+    {
+        $this->description = $description;
+        $this->type = 'string';
+    }
+
+	public function getMinLength()
+	{
+		return $this->minLength;
+	}
+
+	public function setMinLength(int $minLength): void
+	{
+
+		if ($minLength < 0) {
+			throw new \InvalidArgumentException('Min length must be higher that zero');
+		}
+
+		if (null !== $this->maxLength && $minLength > $this->maxLength) {
+			throw new \InvalidArgumentException('Min length higher that max length');
+		}
+
+		$this->minLength = $minLength;
+	}
+
+	public function getMaxLength()
+	{
+		return $this->maxLength;
+	}
+
+	public function setMaxLength(int $maxLength): void
+	{
+
+		if ($maxLength < 1) {
+			throw new \InvalidArgumentException('Max length must be higher that one');
+		}
+
+		if (null !== $this->minLength && $maxLength < $this->minLength) {
+			throw new \InvalidArgumentException('Max length lower that min length');
+		}
+
+		$this->maxLength = $maxLength;
+	}
+
+	public function getPatter()
+	{
+		return $this->patter;
+	}
+
+	public function setPatter($patter): void
+	{
+
+		// Taken from https://github.com/opis/json-schema/blob/19868514d5e4c27553b21c7f0cf6388734c67d18/src/Validator.php
+
+		$match = @preg_match(self::BELL . $patter . self::BELL . 'u', "dummy_data");
+
+		if ($match === false) {
+			throw new \InvalidArgumentException('Pattern '.$patter.' is not a valid regex');
+		}
+
+		$this->patter = $patter;
+
+	}
+
+}
---
+++ test/TypeStringTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Musement\JsonSchema\Test;
+
+use Musement\JsonSchema\Types\Libs\Range;
+use Musement\JsonSchema\Types\TypeInteger;
+use Musement\JsonSchema\Types\TypeString;
+
+final class TypeStringTest extends \PHPUnit\Framework\TestCase
+{
+
+	public function testTypeStringIsCreatedWithTypeString()
+	{
+		$mySchema = new TypeString("My sting");
+		$this->assertSame("string", $mySchema->getType());
+	}
+
+	public function testTypeStringThrowExceptionIfMinLengthIsHigherThanMaxLength()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$myString = new TypeString("My string");
+		$myString->setMaxLength(10);
+		$myString->setMinLength(11);
+	}
+
+	public function testTypeStringThrowExceptionIfMaxLengthIsLowerThanMinLength()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$myString = new TypeString("My string");
+		$myString->setMinLength(11);
+		$myString->setMaxLength(10);
+	}
+
+	public function testTypeStringCanBeCreateWithSameMaxLengthAndMinLength()
+	{
+		$myString = new TypeString("My string");
+		$myString->setMinLength(10);
+		$myString->setMaxLength(10);
+
+		$this->assertSame(10, $myString->getMaxLength());
+		$this->assertSame(10, $myString->getMinLength());
+	}
+
+	public function testSetInvalidPatterThrowException()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$myString = new TypeString("My string");
+		$myString->setPatter("^(0-9]{3}))?[0-9]{3}-[0-9]{4}$");
+	}
+
+	public function testSetNegativeMinLenghtThrowException()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$myString = new TypeString("My string");
+		$myString->setMinLength(-1);
+	}
+
+	public function testSetMaxLenghtLowerThanOneThrowException()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$myString = new TypeString("My string");
+		$myString->setMaxLength(0);
+	}
+
+}
