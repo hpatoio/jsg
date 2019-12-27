fcbbbe7a38b04d7b539a0b08d4b2f96f8d116f9e
Simone Fumagalli <simone.fumagalli@musement.com>
2019-12-27 13:03:35 +0100

    Fixed PHPStan on PropertiesAwareTrait

--- src/Traits/PropertiesAwareTrait.php
+++ src/Traits/PropertiesAwareTrait.php
@@ -8,28 +8,36 @@ use JMS\Serializer\Annotation as JMS;
 trait PropertiesAwareTrait
 {
     /**
+     * @var array<JsonSchemaType>
      * @JMS\SkipWhenEmpty()
      * @JMS\Type("array<string, Hpatoio\JsonSchema\Types\JsonSchemaType>")
      */
     protected $properties = [];
 
     /**
+     * @var array<string>
      * @JMS\SkipWhenEmpty()
      * @JMS\Type("array")
      */
     protected $required = [];
 
+    /**
+     * @return array<JsonSchemaType>
+     */
     public function getProperties(): array
     {
         return $this->properties;
     }
 
+    /**
+     * @return array<string>
+     */
     public function getRequired(): array
     {
         return $this->required;
     }
 
-    public function addProperty(JsonSchemaType $property, bool $required = false)
+    public function addProperty(JsonSchemaType $property, bool $required = false): void
     {
         if (isset($this->properties[$property->getName()])) {
             throw new \InvalidArgumentException('Property '.$property->getName().' already present.');
